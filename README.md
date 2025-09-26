9ab97fb4b6bf5a42e3a3de0908517467df80eb4c
Valerii Koniushenko <Valerii.Koniushenko@gmail.com>
2025-09-26 15:18:18 +0200

    added tests

--- README.md
+++ README.md
@@ -1,21 +1,29 @@
 ```
-------------------------------------------------------------------------------
-Benchmark                                    Time             CPU   Iterations
-------------------------------------------------------------------------------
-CustomStrStr                             0.202 ns        0.201 ns   3442169216
-StdStrStr                                0.202 ns        0.202 ns   3433508949
-CustomCaseLessStrStr                      33.0 ns         33.0 ns     20638661
-CustomCaseLessStrStrWithPredictions       33.5 ns         33.5 ns     20774945
-NonStaticLambda                          39924 ns        39820 ns        17598
-StaticLambda                             39668 ns        39667 ns        17526
-ArrWithoutDefaultZero                    40315 ns        40198 ns        17542
-ArrWithDefaultZero                       39949 ns        39929 ns        17543
-StructWithoutDefaultZero                 27149 ns        27087 ns        25696
-StructWithDefaultZero                    33636 ns        33593 ns        20817
-JustVariableAdd                          13365 ns        13319 ns        52552
-HeapPointerAdd                           14104 ns        14039 ns        49244
-HeadRestrictPointerAdd                   13662 ns        13607 ns        51040
-HeapSmartPointerAdd                      13928 ns        13903 ns        49126
-StackPointerAdd                          13392 ns        13345 ns        53026
-StackRefAdd                              13961 ns        13933 ns        50962
+------------------------------------------------------------------------------------------
+Benchmark                                                Time             CPU   Iterations
+------------------------------------------------------------------------------------------
+FindFirstStruct_CFor                                 0.227 ns        0.227 ns   3092790752
+FindFirstStruct_RangeBasedFor_RefCompression         0.226 ns        0.226 ns   3084435732
+FindFirstStruct_RangeBasedFor_NonRefCompression      0.226 ns        0.226 ns   3092424989
+FindFirstStruct_Find                                 30878 ns        30876 ns        22632
+FindFirstStruct_FindIf                               23912 ns        23908 ns        29185
+FindFirstStruct_RangesFindIf                         31331 ns        31327 ns        22704
+FindFirstStruct_FindIfWithStaticPred                 24371 ns        24369 ns        29148
+FindFirstStruct_RangesFindIfWithStaticPred           30992 ns        30988 ns        22422
+CustomStrStr                                          7.19 ns         7.19 ns     96685861
+StdStrStr                                            0.228 ns        0.227 ns   3095542951
+CustomCaseLessStrStr                                  37.4 ns         37.3 ns     18531536
+CustomCaseLessStrStrWithPredictions                   37.3 ns         37.2 ns     18880416
+NonStaticLambda                                      89309 ns        89204 ns         7644
+StaticLambda                                         88987 ns        88982 ns         7873
+ArrWithoutDefaultZero                                88948 ns        88940 ns         7872
+ArrWithDefaultZero                                   88990 ns        88978 ns         7865
+StructWithoutDefaultZero                             59421 ns        59418 ns        11775
+StructWithDefaultZero                                74350 ns        74340 ns         9433
+JustVariableAdd                                      29807 ns        29803 ns        23525
+HeapPointerAdd                                       30658 ns        30628 ns        22920
+HeadRestrictPointerAdd                               29714 ns        29713 ns        23400
+HeapSmartPointerAdd                                  30537 ns        30533 ns        22985
+StackPointerAdd                                      29659 ns        29659 ns        23616
+StackRefAdd                                          29992 ns        29988 ns        23484
 ```
--- main.cpp
+++ main.cpp
@@ -1,5 +1,135 @@
 #include <cstring>
+#include <iostream>
 #include <benchmark/benchmark.h>
+
+#define SUB_SIZE 1 << 17
+
+struct SomeStruct {
+    const char name[8] = "name";
+    float f = 3.1415f;
+    int i = 0;
+
+    [[nodiscard]] bool operator==(const SomeStruct &other) const { return other.i == i; }
+};
+
+static int whatToFind = 0;
+static const std::vector<SomeStruct> structs = [] {
+    std::vector<SomeStruct> out;
+    out.reserve(SUB_SIZE);
+
+    for (std::size_t i = 0; i < SUB_SIZE; ++i) {
+        out.push_back(SomeStruct());
+        out.back().i = i * 10;
+        out.back().f += i;
+    }
+
+    whatToFind = out.at(out.size() - 10).i;
+
+    return out;
+}();
+
+static void FindFirstStruct_CFor(benchmark::State &state) {
+    for (auto _: state) {
+        const int *p = nullptr;
+        for (std::size_t i = 0; i < structs.size(); ++i) {
+            if (structs.at(i).i == whatToFind) {
+                p = &structs.at(i).i;
+                break;
+            }
+        }
+
+        benchmark::DoNotOptimize(*p + 333);
+    }
+}
+
+static void FindFirstStruct_RangeBasedFor_RefCompression(benchmark::State &state) {
+    for (auto _: state) {
+        const int *p = nullptr;
+        for (auto &&obj: structs) {
+            if (obj.i == whatToFind) {
+                p = &obj.i;
+                break;
+            }
+        }
+        benchmark::DoNotOptimize(*p + 333);
+    }
+}
+
+static void FindFirstStruct_RangeBasedFor_NonRefCompression(benchmark::State &state) {
+    for (auto _: state) {
+        const int *p = nullptr;
+        for (auto &obj: structs) {
+            if (obj.i == whatToFind) {
+                p = &obj.i;
+                break;
+            }
+        }
+        benchmark::DoNotOptimize(*p + 333);
+    }
+}
+
+static void FindFirstStruct_FindIf(benchmark::State &state) {
+    for (auto _: state) {
+        const int *p = nullptr;
+        const auto it = std::find_if(structs.begin(), structs.end(),
+                                     [&](const SomeStruct &obj) { return obj.i == whatToFind; });
+        p = &it->i;
+        benchmark::DoNotOptimize(*p + 333);
+    }
+}
+
+static void FindFirstStruct_FindIfWithStaticPred(benchmark::State &state) {
+    static auto pred = [&](const SomeStruct &obj) { return obj.i == whatToFind; };
+
+    for (auto _: state) {
+        const int *p = nullptr;
+        const auto it = std::find_if(structs.begin(), structs.end(),
+                                     pred);
+        p = &it->i;
+        benchmark::DoNotOptimize(*p + 333);
+    }
+}
+
+static void FindFirstStruct_RangesFindIf(benchmark::State &state) {
+    for (auto _: state) {
+        const int *p = nullptr;
+        const auto it = std::ranges::find_if(structs, [&](const SomeStruct &obj) { return obj.i == whatToFind; });
+        p = &it->i;
+        benchmark::DoNotOptimize(*p + 333);
+    }
+}
+
+static void FindFirstStruct_Find(benchmark::State &state) {
+    SomeStruct s;
+    s.i = whatToFind;
+    for (auto _: state) {
+        const int *p = nullptr;
+        const auto it = std::ranges::find(structs, s);
+        p = &it->i;
+        benchmark::DoNotOptimize(*p + 333);
+    }
+}
+
+static void FindFirstStruct_RangesFindIfWithStaticPred(benchmark::State &state) {
+    static auto pred = [&](const SomeStruct &obj) { return obj.i == whatToFind; };
+
+    for (auto _: state) {
+        const int *p = nullptr;
+        const auto it = std::ranges::find_if(structs, pred);
+        p = &it->i;
+        benchmark::DoNotOptimize(*p + 333);
+    }
+}
+
+BENCHMARK(FindFirstStruct_CFor);
+BENCHMARK(FindFirstStruct_RangeBasedFor_RefCompression);
+BENCHMARK(FindFirstStruct_RangeBasedFor_NonRefCompression);
+BENCHMARK(FindFirstStruct_Find);
+BENCHMARK(FindFirstStruct_FindIf);
+BENCHMARK(FindFirstStruct_RangesFindIf);
+BENCHMARK(FindFirstStruct_FindIfWithStaticPred);
+BENCHMARK(FindFirstStruct_RangesFindIfWithStaticPred);
+
 
 const char *CaselessStrStrFunc(const char *haystack, const char *needle) {
     if (!*needle) return haystack;
@@ -97,7 +227,7 @@ static void NonStaticLambda(benchmark::State &state) {
     for (auto _: state) {
         auto func = []() {
             int arr[1024]{};
-            for (std::size_t i = 0; i < 1 << 16; ++i) {
+            for (std::size_t i = 0; i < SUB_SIZE; ++i) {
                 benchmark::DoNotOptimize(arr[0] = 1 + i);
                 benchmark::DoNotOptimize(arr[66] = 5 * i);
                 benchmark::DoNotOptimize(arr[1023] = 6 * i + 3);
@@ -112,7 +242,7 @@ static void StaticLambda(benchmark::State &state) {
     for (auto _: state) {
         static auto func = []() {
             int arr[1024]{};
-            for (std::size_t i = 0; i < 1 << 16; ++i) {
+            for (std::size_t i = 0; i < SUB_SIZE; ++i) {
                 benchmark::DoNotOptimize(arr[0] = 1 + i);
                 benchmark::DoNotOptimize(arr[66] = 5 * i);
                 benchmark::DoNotOptimize(arr[1023] = 6 * i + 3);
@@ -129,7 +259,7 @@ BENCHMARK(StaticLambda);
 static void ArrWithoutDefaultZero(benchmark::State &state) {
     for (auto _: state) {
         int arr[1024];
-        for (std::size_t i = 0; i < 1 << 16; ++i) {
+        for (std::size_t i = 0; i < SUB_SIZE; ++i) {
             benchmark::DoNotOptimize(arr[0] = 1 + i);
             benchmark::DoNotOptimize(arr[66] = 5 * i);
             benchmark::DoNotOptimize(arr[1023] = 6 * i + 3);
@@ -141,7 +271,7 @@ static void ArrWithoutDefaultZero(benchmark::State &state) {
 static void ArrWithDefaultZero(benchmark::State &state) {
     for (auto _: state) {
         int arr[1024]{};
-        for (std::size_t i = 0; i < 1 << 16; ++i) {
+        for (std::size_t i = 0; i < SUB_SIZE; ++i) {
             benchmark::DoNotOptimize(arr[0] = 1 + i);
             benchmark::DoNotOptimize(arr[66] = 5 * i);
             benchmark::DoNotOptimize(arr[1023] = 6 * i + 3);
@@ -161,7 +291,7 @@ static void StructWithoutDefaultZero(benchmark::State &state) {
     };
 
     for (auto _: state) {
-        for (std::size_t i = 0; i < 1 << 16; ++i) {
+        for (std::size_t i = 0; i < SUB_SIZE; ++i) {
             benchmark::DoNotOptimize(Foo{1, 2});
             Foo f;
             benchmark::DoNotOptimize(f.a = 1 + i);
@@ -178,7 +308,7 @@ static void StructWithDefaultZero(benchmark::State &state) {
     };
 
     for (auto _: state) {
-        for (std::size_t i = 0; i < 1 << 16; ++i) {
+        for (std::size_t i = 0; i < SUB_SIZE; ++i) {
             benchmark::DoNotOptimize(Foo{1, 2});
             Foo f;
             benchmark::DoNotOptimize(f.a = 1 + i);
@@ -194,7 +324,7 @@ BENCHMARK(StructWithDefaultZero);
 static void JustVariableAdd(benchmark::State &state) {
     for (auto _: state) {
         int x = 0;
-        for (std::size_t i = 0; i < 1 << 16; ++i) {
+        for (std::size_t i = 0; i < SUB_SIZE; ++i) {
             benchmark::DoNotOptimize(x += i);
         }
     }
@@ -203,7 +333,7 @@ static void JustVariableAdd(benchmark::State &state) {
 static void HeapPointerAdd(benchmark::State &state) {
     for (auto _: state) {
         int *x = new int;
-        for (std::size_t i = 0; i < 1 << 16; ++i) {
+        for (std::size_t i = 0; i < SUB_SIZE; ++i) {
             benchmark::DoNotOptimize(*x += i);
             benchmark::ClobberMemory();
         }
@@ -214,7 +344,7 @@ static void HeapPointerAdd(benchmark::State &state) {
 static void HeapSmartPointerAdd(benchmark::State &state) {
     for (auto _: state) {
         auto x = std::make_unique<int>();
-        for (std::size_t i = 0; i < 1 << 16; ++i) {
+        for (std::size_t i = 0; i < SUB_SIZE; ++i) {
             benchmark::DoNotOptimize(*x += i);
             benchmark::ClobberMemory();
         }
@@ -225,7 +355,7 @@ static void StackPointerAdd(benchmark::State &state) {
     for (auto _: state) {
         int a = 0;
         int *x = &a;
-        for (std::size_t i = 0; i < 1 << 16; ++i) {
+        for (std::size_t i = 0; i < SUB_SIZE; ++i) {
             benchmark::DoNotOptimize(*x += i);
             benchmark::ClobberMemory();
         }
@@ -236,7 +366,7 @@ static void HeadRestrictPointerAdd(benchmark::State &state) {
     for (auto _: state) {
         int a = 0;
         int *__restrict x = &a;
-        for (std::size_t i = 0; i < 1 << 16; ++i) {
+        for (std::size_t i = 0; i < SUB_SIZE; ++i) {
             benchmark::DoNotOptimize(*x += i);
             benchmark::ClobberMemory();
         }
@@ -247,7 +377,7 @@ static void StackRefAdd(benchmark::State &state) {
     for (auto _: state) {
         int a = 0;
         int &x = a;
-        for (std::size_t i = 0; i < 1 << 16; ++i) {
+        for (std::size_t i = 0; i < SUB_SIZE; ++i) {
             benchmark::DoNotOptimize(x += i);
             benchmark::ClobberMemory();
         }
